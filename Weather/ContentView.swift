ee4dd9a765bd1a5d465bdc57b49a93789662b47a
Daniel <daniel.prolite@gmail.com>
2020-08-09 20:42:31 -0500

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Daniel Vazquez on 09/08/20.
+//  Copyright © 2020 Daniel Vazquez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
